09429399b92150219979865c095249b1ed41300a
pedronetoalves <pedronetoalves@hotmail.com>
2025-01-22 00:03:48 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
@@ -12,7 +13,7 @@ import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 
 @RestController
-@RequestMapping(value = "/games")
+@RequestMapping(value = "/games", method = RequestMethod.GET)
 public class GameController 
 {
 
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
@@ -11,6 +12,7 @@ import com.devsuperior.dslist.repositories.GameRepository;
 @Service
 public class GameService {
 
+	@Autowired
 	private GameRepository gameRepository;
 	public List<GameMinDTO>  findAll()
 	{
