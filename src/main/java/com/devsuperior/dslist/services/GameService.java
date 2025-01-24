d863fcb36dd05241a8387f9dd143d118e23c1b2a
pedronetoalves <pedronetoalves@hotmail.com>
2025-01-24 13:11:05 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
-
-
 
 @Service
 public class GameService {
@@ -21,28 +20,20 @@ public class GameService {
 	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
-	public GameDTO findById(Long id)
-	{
-		Game result = gameRepository.findById(id).get();
-		GameDTO dto = new GameDTO(result);
-		return dto;
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
 	}
 	
 	@Transactional(readOnly = true)
-	public List<GameMinDTO>  findAll()
-	{
+	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
+		return result.stream().map(GameMinDTO::new).toList();
 	}
 	
 	@Transactional(readOnly = true)
-	public List<GameMinDTO>  findByList(long ListId)
-	{
-		List<GameMinProjection> result = gameRepository.searchByList(ListId);
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
+	public List<GameMinDTO> findByGameList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(GameMinDTO::new).toList();
 	}
-	
-	
 }
